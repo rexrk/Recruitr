63e5e00549d10d2a768fb291f2a073f337104c7d
Raman Kumar <kraman938@gmail.com>
2025-09-18 16:43:06 +0530

    initial commit

---
+++ src/main/java/com/raman/recruitr/RecruitrApplication.java
@@ -0,0 +1,13 @@
+package com.raman.recruitr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raman/recruitr/RecruitrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raman.recruitr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
